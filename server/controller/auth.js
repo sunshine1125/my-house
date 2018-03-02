3d8cbb8b8a05e9e63e1dcb59cd5060837d64dca2
sunshine1125 <371262808@qq.com>
2018-03-02 14:53:15 +0800

    密码加密

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
 const SALT_ROUNDS = 10;
 const express = require('express');
 const app = express();
@@ -13,36 +13,41 @@ app.set('superSecret', config().databaseConnect().secret);// secret variable
 // user register
 apiRoutes.post('/register', (req, res) => {
     let password = req.body.password;
-    let newUser = new User({
-        username          : req.body.username,
-        password          : req.body.password,
-        email             : req.body.email,
-        confirmation_token: config().getconfirToken(),
-        changePassword    : false,
-        admin             : true
-    });
-    User.findOne({
-        email: req.body.email
-    }, function (err, user) {
-        if (user) {
-            res.json({success: false, message: '该邮箱已经注册，请直接登录！'})
-        } else {
-            // // 生成salt并获取hash值
-            // bcrypt.genSalt(SALT_ROUNDS, (err, salt) => {
-            //     bcrypt.hash()
-            // });
+    let noop = function() {};
+    // 生成salt并获取hash值
+    bcrypt.genSalt(SALT_ROUNDS, (err, salt) => {
+        bcrypt.hash(password, salt, noop, (err, hash) => {
+            // 把hash值赋值给password变量
+            password = hash;
 
-            newUser.save((err, data) => {
-                if (err) {
-                    res.status('405').json({code: 405, msg: err})
+            let newUser = new User({
+                username          : req.body.username,
+                password          : password,// 把加密后的密码存入数据库
+                email             : req.body.email,
+                confirmation_token: config().getconfirToken(),
+                changePassword    : false,
+                admin             : true
+            });
+            User.findOne({
+                email: req.body.email
+            }, function (err, user) {
+                if (user) {
+                    res.json({success: false, message: '该邮箱已经注册，请直接登录！'})
                 } else {
-                    return res.json({
-                        success: true,
-                        message: '注册成功',
+
+                    newUser.save((err, data) => {
+                        if (err) {
+                            res.status('405').json({code: 405, msg: err})
+                        } else {
+                            return res.json({
+                                success: true,
+                                message: '注册成功',
+                            })
+                        }
                     })
                 }
-            })
-        }
+            });
+        })
     });
 
 });
@@ -114,19 +119,31 @@ apiRoutes.get('/checkPassword', (req, res) => {
 });
 
 apiRoutes.post('/singleUser', (req, res) => {
-    console.log(req.body.oldPassword)
     if (req.body.oldPassword) {
         User.findOne({email: req.body.email}, (err, user) => {
-            if (user.password != req.body.oldPassword) {
-                res.json({code: 401, success: false, message: '旧密码输入错误'})
-            } else {
-                User.update({email: req.body.email}, {password: req.body.password}, (err, docs) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    res.status('200').json({code: 200, success: true, message: '密码修改成功'})
-                });
-            }
+            let oldPassword = req.body.oldPassword;
+            bcrypt.compare(oldPassword, user.password, (err, isMatch) => {
+                if (!isMatch) {
+                    res.json({code: 401, success: false, message: '旧密码输入错误'})
+                } else {
+                    let password = req.body.password;
+                    let noop = function() {};
+                    // 生成salt并获取hash值
+                    bcrypt.genSalt(SALT_ROUNDS, (err, salt) => {
+                        bcrypt.hash(password, salt, noop, (err, hash) => {
+                            // 把hash值赋值给password变量
+                            password = hash;
+                            console.log(password);
+                            User.update({email: req.body.email}, {password: password}, (err, docs) => {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                res.status('200').json({code: 200, success: true, message: '密码修改成功'})
+                            });
+                        })
+                    })
+                }
+            })
         })
     }
 });
@@ -152,6 +169,7 @@ apiRoutes.put('/canChangePassword', (req, res) => {
 });
 
 apiRoutes.post('/authentication', (req, res) => {
+    let password = req.body.password;
     User.findOne({
         email: req.body.username
     }, function (err, user) {
@@ -159,26 +177,28 @@ apiRoutes.post('/authentication', (req, res) => {
         if (!user) {
             res.json({success: false, check: true, message: '验证失败，没有找到用户！'})
         } else if (user) {
-            if (user.password != req.body.password) {
-                res.json({success: false, check: true, message: '密码输入错误'})
-            } else if (user.confirmation_token) {
-                res.json({success: false, check: false, message: '邮箱验证失败，请重新验证！'});
-            } else {
-                const payload = {
-                    admin: user.admin
-                };
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if (!isMatch) {
+                    res.json({success: false, check: true, message: '密码输入错误'})
+                } else if (user.confirmation_token) {
+                    res.json({success: false, check: false, message: '邮箱验证失败，请重新验证！'});
+                } else {
+                    const payload = {
+                        admin: user.admin
+                    };
 
-                let token = jwt.sign(payload, app.get('superSecret'), {
-                    // expiresInMinutes: 1440  // expires in 24 hours
-                });
+                    let token = jwt.sign(payload, app.get('superSecret'), {
+                        // expiresInMinutes: 1440  // expires in 24 hours
+                    });
 
-                // return the information including token as JSON
-                return res.json({
-                    success: true,
-                    message: '登录成功',
-                    token  : token
-                })
-            }
+                    // return the information including token as JSON
+                    return res.json({
+                        success: true,
+                        message: '登录成功',
+                        token  : token
+                    })
+                }
+            })
         }
     })
 });
