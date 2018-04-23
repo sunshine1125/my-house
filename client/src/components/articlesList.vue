c26e5dbf76c68cf3e50d250263dae98da2158f19
sunshine1125 <371262808@qq.com>
2018-04-23 18:33:36 +0800

    添加前端主页面

---
+++ client/src/components/articlesList.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="articlesList">
+    <h1 class="header">所有文章</h1>
+    <div>
+      <el-row class="box-card" v-for="article in articlesList">
+        <el-card>
+          <!--<img src="" alt="" class="image">-->
+          <div>
+            <h1 @click="getDetail(article._id)" class="articleTitle">{{article.title}}</h1>
+            <p class="content" v-html="getContent(article.content)"></p>
+            <div class="bottom clearfix">
+              <time class="time">{{article.date}}</time>
+              <el-button type="text" class="button" @click="getDetail(article._id)">阅读全文 >></el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-row>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  import removeMd from 'remove-markdown'
+  export default {
+    name   : 'articlesList',
+    data() {
+      return {
+        articlesList: [],
+      }
+    },
+    mounted: function () {
+      this.$http.get('/api/post/getAllArticles').then((res) => {
+        if (res.data.success && res.data.data) {
+          res.data.data.forEach((data) => {
+            data.date = data.date.substring(0, 10)
+          })
+          this.articlesList = res.data.data;
+        }
+
+      })
+    },
+    methods: {
+      getContent(content) {
+        return removeMd(content);
+      },
+      getDetail(id) {
+        this.$router.push('/detail/?id=' + id);
+      }
+    },
+    components: {
+
+    }
+  }
+</script>
+<style scoped>
+  .articlesList {
+    width: 40%;
+    margin: auto;
+  }
+  .articlesList .header {
+    padding: 40px 0 30px 0;
+  }
+  .time {
+    font-size: 13px;
+    color: #999;
+  }
+
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .button {
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 100%;
+    display: block;
+    /*padding: 20px 0;*/
+    margin: 20px 0 0 0;
+    text-align: left;
+  }
+  .box-card .articleTitle {
+    display: inline-block;
+  }
+  .box-card .articleTitle:hover{
+    cursor: pointer;
+    color: rgb(64, 158, 255);
+
+  }
+  .box-card .content {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+</style>
