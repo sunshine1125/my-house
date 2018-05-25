ba1fb0a5d92baad8d67d32a7c5529480b49338fb
sunshine1125 <371262808@qq.com>
2018-05-25 14:02:27 +0800

    引入qingstor存储

---
+++ server/tools/qingstor.js
@@ -0,0 +1,62 @@
+/**
+ * Document: https://docs.qingcloud.com/qingstor/sdk/js/index.html
+ */
+
+module.exports = { uploadObject };
+
+function init(env) {
+  let QingStor = require('qingstor-sdk').QingStor;
+  let Config = require('qingstor-sdk').Config;
+  let userConfig = new Config().loadDefaultConfig();
+  userConfig.access_key_id = env.access_id;
+  userConfig.secret_access_key = env.access_key;
+
+  return new QingStor(userConfig);
+}
+
+/**
+ * 上传文件到指定Bucket
+ * @param service
+ * @param path
+ * @param uploadFile
+ */
+function uploadObject(env, path, uploadFile) {
+  let service = init(env);
+  let bucket = service.Bucket(env.bucket_name, env.location);
+
+  return new Promise((resolve, reject) => {
+    bucket.putObject(
+      path,
+      {
+        body: require('fs').readFileSync(uploadFile),
+      },
+      (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(data);
+      }
+    );
+  });
+}
+
+/**
+ * 列出所有bucket,暂时用不上
+ * @param service
+ */
+function listBuckets(service) {
+  service.listBuckets({ location: LOCATION }, (err, data) => {
+    console.log(data.buckets);
+  });
+}
+
+/**
+ * 列出bucket中所有的Object文件
+ * @param service
+ */
+function listObjectsOfBucket(service) {
+  let bucket = service.Bucket(BUCKET_NAME, LOCATION);
+  bucket.listObjects({}, (err, data) => {
+    console.log(data);
+  });
+}
